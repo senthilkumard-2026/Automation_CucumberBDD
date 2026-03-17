b039cca9ae9b5061e79e05a4ac9249282acc2b87
SenthilKumar-D <senthilkumar104096@gmail.com>
2026-03-17 15:50:48 +0530

    Added steps in AutomationExcercise Test

--- src/test/java/stepDefinitions/AutomationExerciseStepDef.java
+++ src/test/java/stepDefinitions/AutomationExerciseStepDef.java
@@ -47,4 +47,62 @@ public class AutomationExerciseStepDef {
 		Assert.assertTrue(registerPage.verifyEnterAccInfoMessage().contains(enterAccMsg));
 	}
 
+	@When("User fills account details")
+	public void user_fills_account_details() {
+		registerPage.fillAccountDetails("Test@1234", "9", "March", "2000");
+	}
+
+	@When("User selects newsletter checkbox")
+	public void user_selects_newsletter_checkbox() {
+		registerPage.selectNewsLetter();
+	}
+
+	@When("User selects special offers checkbox")
+	public void user_selects_special_offers_checkbox() {
+		registerPage.selectOffers();
+	}
+
+	@When("User fills address details")
+	public void user_fills_address_details() {
+		registerPage.fillAddress("Hitman", "Rohit", "Wankhede", "India", "Maharastra", "Bombay", "600247",
+				"9124745247");
+	}
+
+	@When("User clicks Create Account button")
+	public void user_clicks_create_account_button() {
+		registerPage.clickCreateAccount();
+	}
+
+	@When("User clicks Continue button")
+	public void user_clicks_continue_button() {
+		registerPage.clickContinue();
+	}
+
+	@When("User clicks Delete Account button")
+	public void user_clicks_delete_account_button() {
+		registerPage.deleteAccount();
+	}
+
+	@Then("User verifies {string} message is visible")
+	public void user_verifies_message_is_visible(String accountCreatedMsg) {
+		Assert.assertTrue(registerPage.accountCreatedMessage().contains(accountCreatedMsg));
+	}
+
+	@Then("User verifies that message {string} is visible")
+	public void user_verifies_that_message_is_visible(String loggedAsUserMsg) {
+		boolean status = registerPage.verifyLoggedInMessage(loggedAsUserMsg);
+
+		if (status) {
+			System.out.println("Logged in message is visible");
+		} else {
+			System.out.println("Logged in message is not visible");
+		}
+	}
+
+	@Then("User verifies that {string} is populated")
+	public void user_verifies_that_is_populated(String acctDeletedMsg) {
+		Assert.assertTrue(registerPage.accountDeletedMessage().contains(acctDeletedMsg));
+
+	}
+
 }
